26b7ab0a7e3d374bc2e26e86cd2b90a916a830fa
iamaliv3 <77809257+iamaliv3@users.noreply.github.com>
2022-09-15 15:22:22 -0600

    Update SyntaxAnalyzer.scala

--- activity_05_parser/src/SyntaxAnalyzer.scala
+++ activity_05_parser/src/SyntaxAnalyzer.scala
@@ -42,53 +42,107 @@ class SyntaxAnalyzer(private var source: String) {
   private def parseExpr(): Node = {
 
     // TODO: create a new (tree) node with lexeme-label "expression"
-
+    val node = new Node(new Lexeme(label="expression"))
+    
     // TODO: parse a term
-
+    node.add(parseTerm())
+    
     // TODO: parse an expressionPrime
-
+    node.add(expressionProme())
+    
     // TODO: return the (tree) node created
+    node
   }
 
   // term = factor term'
   private def parseTerm(): Node = {
 
     // TODO: create a new (tree) node with lexeme-label "term"
-
+     val node = new Node(new Lexeme(label="term"))
+    
     // TODO: parse a factor
-
+     node.add(parseFactor())
+    
     // TODO: parse a termPrime
+    node.add(parseTermProme())
 
     // TODO: return the (tree) node created
+    node
   }
 
   // factor = identifier | literal | ´(´ expression ´)´
   private def parseFactor(): Node = {
 
     // TODO: create a new (tree) node with lexeme-label "factor"
-
+    val node = new Node(new Lexeme( label="factor"))
+    
     // TODO: decide whether to add identifier, literal or expression in parentheses branches
-
+    
+    if (getLexeme.token = Token.IDENTIFIER || getLexeme.token == Token.LITERAL) {
+      node.add(new Node (getLexeme))
+      nextLexeme // don't forget to consume lexeme!!!  
+    }
+    else if (getLexeme.token = Token.OPEN_PAR) {
+      node.add(new Node (getLexeme))
+      nextLexeme // consuming the open par
+      node.add(parseExpr())
+      if (getLexeme.token = Token.CLOSE_PAR) {
+        node.add(new Node (getLexeme))
+        nextLexeme // consuming the close par
+    }
+    else 
+      throw 
+    
     // TODO: return the (tree) node created
-  }
+    node
+  } : Unit
 
   // term' = ( ´*´ | ´/´ ) factor term' | epsilon
   def parseTermPrime(): Node = {
 
     // TODO: create a new (tree) node with lexeme-label "term"
-
+    val node = new Node(new Lexeme( label="term"))
+    
     // TODO: add as many multiplication or division branches followed by a factor;
+    if (getLexeme.token = Token.MULTIPLICATION || getLexeme.token == Token.DIVISION) {
+      node.add(new Node (getLexeme))
+      nextLexeme // don't forget to consume lexeme!!!  
+      node.add(parseFactor())
+      node.add(parseTermPrime())
+    }
+    else 
+      node.add(new Node( Lexeme( label = "epsilon)))
+    //if (getLexeme.token = Token.OPEN_PAR) {
+      node.add(new Node (getLexeme))
+      nextLexeme // consuming the open par
+      node.add(parseExpr())
+      if (getLexeme.token = Token.CLOSE_PAR) {
+        node.add(new Node (getLexeme))
+        nextLexeme // consuming the close par.
+      }
+      else 
+      throw new Error("Closing
     // use epsilon production as the base-case
 
     // TODO: return the (tree) node created
+    node
   }
 
   // expression' = ( ´+´  | ´-´ ) term expression' | epsilon
   def parseExprPrime(): Node = {
 
     // TODO: create a new (tree) node with lexeme-label "expression"
-
+    val node = new Node(new Lexeme( label="expression"))
+    
     // TODO: add as many addition or subtraction branches followed by a term;
+     if (getLexeme.token = Token.ADDITION || getLexeme.token == Token.SUBTRACTION) {
+      node.add(new Node (getLexeme))
+      nextLexeme // don't forget to consume lexeme!!!  
+      node.add(parseFactor())
+      node.add(parseTermPrime())
+    }
+    else 
+      node.add(new Node( Lexeme( label = "epsilon)))
     // use epsilon production as the base-case
 
     // TODO: return the (tree) node created
